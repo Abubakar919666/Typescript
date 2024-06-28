1bed95d2097d1241d16e8598b8a63d7c3652b21a
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2024-06-28 15:54:57 +0500

    hello-world

---
+++ Day 01/app.js
@@ -0,0 +1,7 @@
+"use strict";
+// Infering Types, 
+//take your cursor on the variable name
+let user = "ali";
+console.log(user);
+let message = "Hello World";
+console.log(message);
---
+++ Day 01/app.ts
@@ -0,0 +1,9 @@
+// Infering Types, 
+//take your cursor on the variable name
+let user = "ali";
+console.log(user);
+
+
+
+let message = "Hello World";
+console.log(message);
